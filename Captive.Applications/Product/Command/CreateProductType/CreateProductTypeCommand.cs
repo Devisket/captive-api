5950307580c98a521882c9703b302281a1bf099a
helbertpernecita <helbert.pernecita@gmail.com>
2025-04-21 22:31:24 +0800

    add product sequence column to products

--- Captive.Applications/Product/Command/CreateProductType/CreateProductTypeCommand.cs
+++ Captive.Applications/Product/Command/CreateProductType/CreateProductTypeCommand.cs
@@ -8,5 +8,6 @@ namespace Captive.Applications.Product.Command.CreateProductType
         public Guid BankId { get; set; }
         public Guid? ProductId { get; set; }
         public required string ProductName { get; set; }
+        public required int ProductSequence { get; set; }
     }
 }
