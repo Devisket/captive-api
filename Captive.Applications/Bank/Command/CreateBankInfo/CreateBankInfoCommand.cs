07002c45008713db4f2418a2089a38807c4f038a
edison.esguerra <edison.esguerra@devisket.com>
2025-09-08 05:37:20 +0800

    fix issue on doblock

--- Captive.Applications/Bank/Command/CreateBankInfo/CreateBankInfoCommand.cs
+++ Captive.Applications/Bank/Command/CreateBankInfo/CreateBankInfoCommand.cs
@@ -7,7 +7,6 @@ namespace Captive.Applications.Bank.Command.CreateBankInfo
         public Guid? Id { get; set; }
         public required string BankName { get; set; }
         public required string ShortName { get; set; }
-        public required string Description { get; set; }
         public string? AccountNumberFormat {  get; set; }
     }
 }
