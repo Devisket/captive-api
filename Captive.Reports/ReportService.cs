768b17b76dc2ff5b3e25a2241e67867cb78bd274
Edison Esguerra <edison.esguerra@devisket.com>
2026-09-07 02:53:14 +0800

    chore: fix the doblock quantity

--- Captive.Reports/ReportService.cs
+++ Captive.Reports/ReportService.cs
@@ -79,7 +79,7 @@ namespace Captive.Reports
                             SeriesPattern = check.CheckInventory!.SeriesPatern,
                             AccountNumberFormat = branches.First().BankInfo.AccountNumberFormat,
                             ProductSequence = formCheck.Product.ProductSequence,
-                            FormCheckQuantity = formCheck.Quantity * check.Quantity,
+                            FormCheckQuantity = check.Quantity,
                         }
                     );
                 }
